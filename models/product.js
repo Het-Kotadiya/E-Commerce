a80d41fd52242d7e7dc84674e40ad7e04cd4cf55
Het-Kotadiya <hetkotadiya0303@gmail.com>
2024-01-20 14:23:54 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,28 +1,19 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const productSchema = new Schema({
-    title: {
-        type: String,
-        required: true
+const productSchema = new Schema(
+    {
+        title: { type: String, required: true, unique: true },
+        desc: { type: String, required: true },
+        img: { type: String, required: true },
+        categories: { type: Array },
+        size: { type: Array },
+        color: { type: Array },
+        price: { type: Number, required: true },
+        inStock: { type: Boolean, default: true },
     },
-
-    description: {
-        type: String
-    },
-
-    image: {
-        url: String
-    },
-
-    price: {
-        type: Number
-    },
-
-    category: {
-        type: String
-    }
-})
+    { timestamps: true }
+);
 
 
 const product = mongoose.model('Product', productSchema)
